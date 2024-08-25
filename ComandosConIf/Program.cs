3515fa69a0eb3b905075fdfe651671de1f1c5a6e
user <user@DESKTOP-MEO67AI>
2024-08-25 12:20:11 -0600

    Agregar archivos de proyecto.

---
+++ ComandosConIf/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un número entre 1 y 12:");
+        int mes = int.Parse(Console.ReadLine());
+
+        if (mes == 1 || mes == 2 || mes == 3)
+        {
+            Console.WriteLine("Verano");
+        }
+        else if (mes == 4 || mes == 5 || mes == 6)
+        {
+            Console.WriteLine("Otoño");
+        }
+        else if (mes == 7 || mes == 8 || mes == 9)
+        {
+            Console.WriteLine("Primavera");
+        }
+        else
+        {
+            Console.WriteLine("Invierno");
+        }
+    }
+}
